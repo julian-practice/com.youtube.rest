c4e4f739b87e0eec99d910043af30bb84be6835d
julian-practice <julian_s@comcast.net>
2015-07-23 11:22:57 -0700

    last of episode 5 code

--- src/com/youtube/rest/inventory/V2_inventory.java
+++ src/com/youtube/rest/inventory/V2_inventory.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -33,7 +34,7 @@ import com.youtube.util.ToJSON;
 		 * @return - json array results list from the database
 		 * @throws Exception
 		 */
-		@GET
+	/*	@GET
 		@Produces(MediaType.APPLICATION_JSON)
 		public Response returnBrandParts(
 					@QueryParam("brand") String brand)
@@ -61,7 +62,65 @@ import com.youtube.util.ToJSON;
 			}
 			
 			return Response.ok(returnString).build();
+		} */
+
+		@GET
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response returnErrorOnBrand() throws Exception {
+			
+			return Response.status(400).entity("Error: please specify brand for this search").build();
 		}
 		
+		@Path("/{brand}")
+		@GET
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response returnBrand(
+					@PathParam("brand") String brand) 
+					throws Exception {
+			
+			String returnString = null;
+			
+			JSONArray json = new JSONArray();
+			
+			try {
+				
+				Schema308tube dao = new Schema308tube();
+				
+				json = dao.queryReturnBrandParts(brand);
+				returnString = json.toString();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				return Response.status(500).entity("Server was not able to process your request").build();
+			}
+			
+			return Response.ok(returnString).build();
+		}
 		
+		@Path("/{brand}/{item_number}")
+		@GET
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response returnSpecificBrandItem(
+					@PathParam("brand") String brand,
+					@PathParam("item_number") int item_number) 
+					throws Exception {
+			
+			String returnString = null;
+			
+			JSONArray json = new JSONArray();
+			
+			try {
+				
+				Schema308tube dao = new Schema308tube();
+				
+				json = dao.queryReturnBrandItemNumber(brand, item_number);
+				returnString = json.toString();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				return Response.status(500).entity("Server was not able to process your request").build();
+			}
+			
+			return Response.ok(returnString).build();
+		}
 }
